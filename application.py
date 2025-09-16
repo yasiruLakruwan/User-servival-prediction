f5de7dcfa4d53ca2a01a55d197d027c0cb143504
yasiruLakruwan <yapa1459@gmail.com>
2025-09-16 23:38:54 +0530

    building API

--- application.py
+++ application.py
@@ -1,1 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template,request,jsonify
+import pickle
+import numpy
+import pandas
+
+app = Flask(__name__,template_folder="templates")
+
+MODEL_PATH = "artifacts/models/random_forrest_model.pkl"
+
+with open(MODEL_PATH,'rb') as model_file:
+    model = pickle.load(model_file)
+
+FEATURE_NAMES = ['Age', 'Fare', 'Pclass', 'Sex', 'Embarked', 'Familysize', 'Isalone',
+       'HasCabin', 'Title', 'Pclass_Fare', 'Age_Fare']

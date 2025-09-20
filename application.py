4b1e38b0c9a176fb0d47c640ecf2e63c7a1db904
yasiruLakruwan <yapa1459@gmail.com>
2025-09-20 20:33:02 +0530

    Date drift detection added

--- application.py
+++ application.py
@@ -3,6 +3,13 @@ import pickle
 import numpy
 import pandas
 import pandas as pd
+from src.logger import get_logger
+
+from alibi_detect.cd import KSDrift
+from src.feature_store import RedisFeatures
+from sklearn.preprocessing import StandardScaler
+
+logger = get_logger(__name__) 
 
 app = Flask(__name__,template_folder="templates")
 
@@ -14,6 +21,20 @@ with open(MODEL_PATH,'rb') as model_file:
 FEATURE_NAMES = ['Age', 'Fare', 'Pclass', 'Sex', 'Embarked', 'Familysize', 'Isalone',
        'HasCabin', 'Title', 'Pclass_Fare', 'Age_Fare']
 
+feature_store = RedisFeatures()
+scaler = StandardScaler()
+
+def fit_scaler_on_ref_data():
+    entity_ids = feature_store.get_all_entity_ids()
+    all_features = feature_store.get_batch_features(entity_ids)
+
+    all_features_df = pd.DataFrame.from_dict(all_features,orient='index')[FEATURE_NAMES]
+    scaler.fit(all_features_df)
+
+    return scaler.transform(all_features_df)
+
+historical_data = fit_scaler_on_ref_data()
+ksd = KSDrift(x_ref=historical_data,p_val=0.05)
 
 @app.route('/')
 def home():
@@ -36,6 +57,20 @@ def predict():
         Age_Fare = float(data["Age_Fare"])
 
         features = pd.DataFrame([[Age,Fare,Pclass,Sex,Embarked,Familysize,Isalone,HasCabin,Title,Pclass_Fare,Age_Fare]] , columns=FEATURE_NAMES)
+
+        #### Data Drift detection
+
+        feature_scaled = scaler.transform(features)
+        
+        drift = ksd.predict(feature_scaled)
+        print("Drift Response: ", drift)
+
+        drift_response = drift.get('data',{})
+        is_drift = drift_response.get('is_drift',None)
+
+        if is_drift is not None and is_drift==1:
+            print("Drift detected...")
+            logger.info("Drift detected....")
 
         prediction = model.predict(features)[0]
         result = 'Survived' if prediction ==1 else 'Did Not Survive'
